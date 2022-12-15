9c12e4d8daae9289a75f1c3d7cf2e63ce4a7aa0b
Filippo Sbaiz <sbaizfilippo@gmail.com>
2022-12-15 09:40:47 +0100

    push copy + array object

---
+++ js/script.js
@@ -0,0 +1,164 @@
+function goToNextSlide(){
+
+    if (itemActive < imagesArray.length -1) {
+
+        items[itemActive].classList.remove('active');
+        circles[itemActive].classList.remove('active');
+        thumbnails[itemActive].classList.remove(`active`);
+    
+        itemActive++;
+
+        items[itemActive].classList.add('active');
+        circles[itemActive].classList.add('active');
+        thumbnails[itemActive].classList.add(`active`);
+    
+        }
+    
+        else {
+    
+        items[itemActive].classList.remove('active');
+        circles[itemActive].classList.remove('active');
+        thumbnails[itemActive].classList.remove(`active`);
+
+        itemActive = 0;
+
+        items[itemActive].classList.add('active');
+        circles[itemActive].classList.add('active');
+        thumbnails[itemActive].classList.add(`active`);
+    
+        }
+
+}
+
+//Creo 5 oggetti a dentro un array 
+const imagesArray = [
+    {
+        image:"01.webp",
+        title:"Spiderman",
+        description:"",
+    },
+    {
+        image:"02.webp",
+        title:"",
+        description:"",
+    },
+    {
+        image:"03.webp",
+        title:"",
+        description:"",
+    },
+    {
+        image:"04.webp",
+        title:"",
+        description:"",
+    },
+    {
+        image:"05.webp",
+        title:"",
+        description:"",
+    } 
+]
+
+//Creiamo dinamicamente i div con le immagini del carosello
+let itemsContent = ``;
+let itemsThumbsnails = ``;
+
+for(let i = 0; i < imagesArray.length; i++){
+    itemsContent += `<div class="item">
+        <img src="./img/${imagesArray[i]}">
+    </div>`;
+
+    itemsThumbsnails += `<div class="thumb"><img src="./img/${imagesArray[i]}">
+    </div>`;
+}
+
+//inseriamo le immagini nel div che le deve contenere
+const itemsSlider = document.querySelector('.item-slider');
+itemsSlider.innerHTML += itemsContent;
+
+const thumbnailsPreview = document.querySelector(`.thumbnails`);
+thumbnailsPreview.innerHTML = itemsThumbsnails;
+
+//Prendiamo la prima immagine dell'array e la rendiamo attiva
+
+//const items = document.querySelector('.item'); //ALTERNATIVA
+
+const items = document.getElementsByClassName('item');
+let itemActive = 0;
+
+items[itemActive].classList.add('active');
+
+//rendo attivo anche il primo cerchio di navigazione
+
+const circles = document.getElementsByClassName('circle');
+
+circles[itemActive].classList.add('active');
+
+const thumbnails = document.getElementsByClassName(`thumb`)
+thumbnails[itemActive].classList.add(`active`);
+
+const prev = document.querySelector('.prev');
+const next = document.querySelector('.next');
+
+// **********************************************
+
+next.addEventListener('click', function(){
+
+    goToNextSlide();
+ 
+});
+
+// ***************************************************
+
+prev.addEventListener('click', function(){
+
+    if (itemActive > 0){
+
+        //verifico l'elemento attivo (itemActive)
+        items[itemActive].classList.remove('active');
+        circles[itemActive].classList.remove('active');
+        thumbnails[itemActive].classList.remove(`active`);
+
+        //decremento il suo valore di 1
+        itemActive--;
+
+        //aggiungere la class active al nuovo elemento dell'array items e la vado a rimuovere da quello precedente
+        //stessa cosa per i cerchi
+        items[itemActive].classList.add('active');
+        circles[itemActive].classList.add('active');
+        thumbnails[itemActive].classList.add(`active`);
+
+    }
+
+    else{
+
+        //verifico l'elemento attivo (itemActive)
+        items[itemActive].classList.remove('active');
+        circles[itemActive].classList.remove('active');
+        thumbnails[itemActive].classList.remove(`active`);
+
+        //decremento il suo valore di 1
+        itemActive = imagesArray.length -1;
+
+        //aggiungere la class active al nuovo elemento dell'array items e la vado a rimuovere da quello precedente
+        //stessa cosa per i cerchi
+        items[itemActive].classList.add('active');
+        circles[itemActive].classList.add('active');
+        thumbnails[itemActive].classList.add(`active`);
+
+    }
+
+})
+
+// creo un set interval e al suo interno ci metto IF next per scorrere le img automaticamente
+
+let myInterval = setInterval(goToNextSlide,2000);
+
+document.getElementById(`play`).addEventListener(`click`, function(){
+    myInterval = setInterval(goToNextSlide,2000);
+})
+
+document.getElementById(`stop`).addEventListener(`click`, function(){
+    clearInterval(myInterval);
+})
+
